9ee43d2b2bc2a1fe8d10c09736f13bf10315f4b2
Adam Juhasz <adam@blaqlabs.com>
2015-04-13 15:43:25 -0400

    Initial Commit

---
+++ later/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  later
+//
+//  Created by Adam Juhasz on 4/13/15.
+//  Copyright (c) 2015 Adam Juhasz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
